9e97306d4d33aefe9bf5dffb439ffb7a084f35a6
Nathan Shih <nathan.c.shih@gmail.com>
2014-11-10 19:11:32 -0600

    added Flashcard, Deck, and Folder classes.  Added functionality to add a new flashcard.

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,3 +1,63 @@
+
+
+function Folder() {
+	var decks = [];
+	this.addDeck = function(Deck) {
+		decks.push(Deck);
+	};
+	this.getDecks = function() {
+		return decks;
+	};
+}
+function Deck() {
+	var cards = [];
+	this.addFlashcard = function(Flashcard) {
+		cards.push(Flashcard);
+	};
+	this.shuffleFlashcards = function() {
+		//TODO
+	};
+	this.playCards = function() {
+		return cards;
+	};
+	this.getRandomCard = function() {
+		return cards[Math.floor(Math.random()*items.length)];
+	};
+}
+
+function Flashcard(front,back) {
+	this.front = front;
+	this.back = back;
+
+}
+
+var topFolder = new Folder();
+var currentDeck = new Deck();
+topFolder.addDeck(currentDeck);
+var currentFlashcard;
+
+
+var makingCard = false;
+
+
 $(document).ready(function() {
-	$("div").fadeOut(1000);
+	$("#btnNewCard").click(function() {
+		//Check to make sure the default string is not in the textarea(s), then add both sides to a bank of cards
+		var textFront = $('#frontside').val();
+		var textBack = $('#backside').val();
+		if (textFront === 'Fill in the frontside of your flashcard here!' || textBack === 'Fill in the back side of your flashcard here!' || textFront === "" || textBack ==="") {
+			//display some sort of error message
+			console.log("TODO: error message");
+		}
+		else {
+			//store the shit inside the textarea to a new Flashcard
+			//CHECK TO SEE IF THIS WORKS
+			currentFlashcard = new Flashcard(textFront,textBack);
+			currentDeck.addFlashcard(currentFlashcard);
+		}
+	});
+	
+
+	
 });
+

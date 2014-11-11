8560d9047198e1b9ea1a1d5aadb7e9edb87ba1ac
Nathan Shih <nathan.c.shih@gmail.com>
2014-11-10 21:38:21 -0600

    renamed some files, added more buttons, have Main, Edit, and Preview windows now

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
 // this file opens windows when the chrome app starts.
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.app.window.create('preferences.html', {
-  	'bounds': {
-  		'width':400,
-  		'height': 500
-  	}
-  });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.app.window.create('preferences.html', {
+//   	'bounds': {
+//   		'width':400,
+//   		'height': 500
+//   	}
+//   });
+// });
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('mainpage.html', {
---
+++ js/workhorse.js
@@ -0,0 +1,80 @@
+//Class declarations and functions
+
+function Folder() {
+	var decks = [];
+	this.addDeck = function(Deck) {
+		decks.push(Deck);
+	};
+	this.getDecks = function() {
+		return decks;
+	};
+}
+
+function Deck() {
+	var cards = [];
+	this.addFlashcard = function(Flashcard) {
+		cards.push(Flashcard);
+	};
+	this.shuffleFlashcards = function() {
+		//TODO
+	};
+	this.playCards = function() {
+		return cards;
+	};
+	this.getRandomCard = function() {
+		return cards[Math.floor(Math.random()*items.length)];
+	};
+}
+
+function Flashcard(front,back) {
+	this.front = front;
+	this.back = back;
+
+}
+
+
+
+//Initialization stuff
+
+
+var topFolder = new Folder();
+var currentDeck = new Deck();
+topFolder.addDeck(currentDeck);
+var currentFlashcard;
+
+
+var makingCard = false;
+
+
+//Jquery Stuff
+//btns- btnPlayDeck, btnOpenCardEditor, btnCreateCard, btnOpenPreview, btnClosePreview
+$(document).ready(function() {
+	$("#btnOpenCardEditor").click(function() {
+		//Check to make sure the default string is not in the textarea(s), then add both sides to a bank of cards
+		console.log("open a new mkFlashcard window");
+		//if we are not making a card at the moment
+
+			makingCard = true;
+			chrome.app.window.create('mkFlashcard.html', {
+				'bounds': {
+					'width':400,
+					'height': 500
+				},
+				'id':"mkFlashcardWindow"
+			});
+	});
+
+	$("#btnPlayDeck").click(function() {
+		console.log("play the deck");
+	});
+
+	$('btnCreateCard').click(function() {
+		
+	});
+
+
+
+});
+
+
+
